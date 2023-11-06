ebe92ec6c4adfa966f4cbc88a3b11c22c5060884
unknown <https://github.com/Likhithacodes/react1.git>
2023-11-06 21:35:34 +0530

    first commit

---
+++ src/components/carousel.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import bgImg1 from '../assets/img1.svg';
+import bgImg2 from '../assets/img2.svg';
+import bgImg3 from '../assets/img3.svg';
+export default function Carousel(){
+    return(
+        
+            <div className="col-2 col-sm-5 col-md-6">
+            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src={bgImg1} class="d-block w-100" alt="..."/>
+                </div>
+                <div class="carousel-item">
+                <img src={bgImg2} class="d-block w-100" alt="..."/>
+                </div>
+                <div class="carousel-item">
+                <img src={bgImg3} class="d-block w-100" alt="..."/>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            </div>
+        </div>
+      
+
+    );
+}
